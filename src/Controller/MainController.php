8f5e42f1ce69d7c16fb21cdfbbef9aa7f926578c
kgardie2021 <kevin.gardie2021@campus-eni.fr>
2022-03-29 14:02:23 +0200

    MAJ css + blocs

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('', name: "main_home")]
-    public function home(){
+    #[Route('', name: "main_accueil")]
+    public function accueil(){
         return $this->render('main/home.html.twig');
     }
 }
